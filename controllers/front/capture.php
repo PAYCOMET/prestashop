f97471a8ce1943ad32dc17e49da70a4145fa201a
Jose Ramón García <jrgarcia@paycomet.com>
2021-03-29 14:11:38 +0200

    Depuración de código redirección URLOK cuando es frictionless

--- controllers/front/capture.php
+++ controllers/front/capture.php
@@ -321,9 +321,20 @@ class PaytpvCaptureModuleFrontController extends ModuleFrontController
                         $merchantData
                     );
 
-                    $salida = $URLKO;
-                    if ($createSubscriptionResponse->challengeUrl != "") {
+                    // Hay challenge
+                    if (isset($createSubscriptionResponse->challengeUrl) &&
+                        $createSubscriptionResponse->challengeUrl != ""
+                    ) {
                         $salida = $createSubscriptionResponse->challengeUrl;
+                    // Frictionless
+                    } elseif (isset($createSubscriptionResponse->errorCode) &&
+                        $createSubscriptionResponse->errorCode == 0 &&
+                        isset($createSubscriptionResponse->authCode) &&
+                        $createSubscriptionResponse->authCode != "") {
+                        $salida = $URLOK;
+                    // Error
+                    } else {
+                        $salida = $URLKO;
                     }
                 } else {
                     try {
@@ -350,10 +361,20 @@ class PaytpvCaptureModuleFrontController extends ModuleFrontController
                             $notifyDirectPayment
                         );
 
-                        $salida = $URLKO;
-
-                        if ($executePurchaseResponse->challengeUrl != "") {
+                        // Hay challenge
+                        if (isset($executePurchaseResponse->challengeUrl) &&
+                            $executePurchaseResponse->challengeUrl != ""
+                        ) {
                             $salida = $executePurchaseResponse->challengeUrl;
+                        // Frictionless
+                        } elseif (isset($executePurchaseResponse->errorCode) &&
+                            $executePurchaseResponse->errorCode == 0 &&
+                            isset($executePurchaseResponse->authCode) &&
+                            $executePurchaseResponse->authCode != "") {
+                            $salida = $URLOK;
+                        // Error
+                        } else {
+                            $salida = $URLKO;
                         }
                     } catch (exception $e) {
                         $salida = $URLKO;
@@ -436,9 +457,20 @@ class PaytpvCaptureModuleFrontController extends ModuleFrontController
                     $merchantData
                 );
 
-                if ($createSubscriptionResponse->challengeUrl != "") {
+                // Hay challenge
+                if (isset($createSubscriptionResponse->challengeUrl) &&
+                    $createSubscriptionResponse->challengeUrl != ""
+                ) {
                     Tools::redirect($createSubscriptionResponse->challengeUrl);
                     exit;
+                // Frictionless
+                } elseif (isset($createSubscriptionResponse->errorCode) &&
+                    $createSubscriptionResponse->errorCode == 0 &&
+                    isset($createSubscriptionResponse->authCode) &&
+                    $createSubscriptionResponse->authCode != "") {
+                    Tools::redirect($URLOK);
+                    exit;
+                // Error
                 } else {
                     $charge = array();
                     $charge["DS_RESPONSE"] = ($createSubscriptionResponse->errorCode > 0)? 0 : 1;
@@ -482,8 +514,18 @@ class PaytpvCaptureModuleFrontController extends ModuleFrontController
                     $charge["DS_RESPONSE"] = ($executePurchaseResponse->errorCode > 0)? 0 : 1;
                     $charge['DS_ERROR_ID'] = $executePurchaseResponse->errorCode;
 
-                    if ($executePurchaseResponse->challengeUrl != "") {
+                    // Hay challenge
+                    if (isset($executePurchaseResponse->challengeUrl) &&
+                        $executePurchaseResponse->challengeUrl != ""
+                    ) {
                         Tools::redirect($executePurchaseResponse->challengeUrl);
+                        exit;
+                    // Frictionless
+                    } elseif (isset($executePurchaseResponse->errorCode) &&
+                        $executePurchaseResponse->errorCode == 0 && 
+                        isset($executePurchaseResponse->authCode) &&
+                        $executePurchaseResponse->authCode != "") {
+                        Tools::redirect($URLOK);
                         exit;
                     }
                 } catch (exception $e) {
@@ -504,7 +546,9 @@ class PaytpvCaptureModuleFrontController extends ModuleFrontController
                     $userInteraction
                 );
 
-                if (isset($charge["DS_CHALLENGE_URL"]) && $charge["DS_CHALLENGE_URL"] != "" && $charge["DS_CHALLENGE_URL"] != "0") {
+                if (isset($charge["DS_CHALLENGE_URL"]) &&
+                    $charge["DS_CHALLENGE_URL"] != "" &&
+                    $charge["DS_CHALLENGE_URL"] != "0") {
                     Tools::redirect(urldecode($charge["DS_CHALLENGE_URL"]));
                     exit;
                 }
