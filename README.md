ed162d97642567a9bc56d0127c84c670889ca464
Carlos Babiano <cbabiano@paycomet.com>
2022-03-11 10:32:40 +0100

    Añadida opción pago DCC

--- README.md
+++ README.md
@@ -60,15 +60,6 @@ Todas las suscripciones mostrarán el estado actual:
 
 ## Release Notes
 
-> 7.7.10: 
-
-- Mejoras de código 
-
-> 7.7.9: 
-
-- Fix APM Klarna
-- Trim para la configuración
-
 > 7.7.8: 
 
 - Fix APM Klarna
---
+++ upgrade/install-7-7.11.php
@@ -0,0 +1,22 @@
+<?php
+/**
+*
+*  @author     PAYCOMET <info@paycomet.com>
+*  @copyright  2021 PAYCOMET S.L.U
+*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*/
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+function upgrade_module_7_7_11()
+{
+    try {
+        Db::getInstance()->execute('ALTER TABLE `'._DB_PREFIX_.'paytpv_terminal` 
+	    ADD COLUMN `dcc` INT(2) UNSIGNED NOT NULL DEFAULT 0
+        ');
+    } catch (exception $e) {
+    }
+    return true;
+}
